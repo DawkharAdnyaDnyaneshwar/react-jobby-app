550a8fe3273808dbaa70d0e47934e42eadb2ac70
Adnya <adnyadawkhar2005@gmail.com>
2025-11-18 13:55:51 +0000

    Added Not Found  Route

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const NotFound = () => (
+    <div className="not-found-container">
+    <img src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png" className="not-found-img" alt="not found"/>
+    <h2 className="not-found-heading">Page Not Found</h2>
+    <p className="not-found-description">We are sorry, the page you requested could not be found</p>
+    </div>
+)
+
+export default NotFound
