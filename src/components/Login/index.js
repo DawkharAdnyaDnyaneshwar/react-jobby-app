738ef848717aff682a1000afaf95c26fbd6c5ad7
Adnya <adnyadawkhar2005@gmail.com>
2025-11-18 13:49:46 +0000

    Added Login Route

---
+++ src/components/Login/index.js
@@ -0,0 +1,93 @@
+import './index.css'
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+
+class Login extends Component {
+  state = {username: '', password: '', showErrMsg: false, errMsg: ''}
+  
+  onSubmit = jwtToken => {
+    const {history} = this.props
+    Cookies.set("jwt_token",jwtToken,{expires: 30})
+    history.replace("/")
+  }
+
+  onsubmitFailure = (errMsg) => {
+    this.setState({showErrMsg: true, errMsg})
+  }
+
+  onSubmit = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username,password}
+    const url = "https://apis.ccbp.in/login"
+    const options = {
+        method: "POST",
+        body: JSON.stringify(userDetails)
+    }
+    const response = await fetch(url, options)
+    if(response.ok) {
+        const data = await response.json()
+        this.onSubmitSuccess(data.jwt_token)
+    } else {
+        this.onsubmitFailure(data.error_msg)
+    }
+  }
+  
+  changeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  changePassword = event => {
+    this.setState({password: event,target,value})
+  }
+  
+  renderUsername = () => {
+    const {username} = this.state
+    return (
+        <>
+        <label className="input-label" htmlFor="username">USERNAME</label>
+        <input id="username" type="text" placeholder="Username" value={username} onChange={this.changeUsername} />
+        </>
+    )
+  }
+
+  renderPassword = () => {
+    const {password} = this.state
+    return (
+        <>
+        <label className="input-label" htmlFor="password">PASSWORD</label>
+        <input id="password" type="text" placeholder="Password" value={password} onChange={this.changePassword} />
+        </>
+    )
+  }
+
+
+  render() {
+    const {errMsg, showErrMsg} = this.state
+    const jwtToken = Cookies.get('jwt-token')
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+
+    return (
+      <div className="login-form-container">
+        <form className="form-container" onSubmit={this.submitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            className="website-logo"
+            alt="website logo"
+          />
+          <div className="input-container">{this.renderUsername()}</div>
+          <div className="input-container">{this.renderPassword()}</div>
+          <button className="login-button" type="submit">
+            Login
+          </button>
+          {showErrMsg && <p className="error-message">*{errMsg}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login
